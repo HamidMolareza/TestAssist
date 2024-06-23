a92c7f9f6a9e9b1b9b871c23a0d322671f1c27b8
HamidMolareza <molareza@protonmail.com>
2024-06-23 23:41:41 +0330

    feat: add `MockDbContext`

--- src/TestAssist.Test/MockExtensionsTests.cs
+++ src/TestAssist.Test/MockExtensionsTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using TestAssist;
+using MockExtensions = TestAssist.MockExtensions;
 
 namespace TestProject;
 
@@ -26,7 +27,56 @@ public class MockExtensionsTests {
         Assert.Equal(data, entities);
     }
 
+    [Fact]
+    public void MockDbContext_GiveCorrectData_ShouldReturnMockedDbContext() {
+        // Arrange
+        var data = new List<TestEntity> {
+            new() { Id = 1, Name = "Test 1" },
+            new() { Id = 2, Name = "Test 2" },
+            new() { Id = 3, Name = "Test 3" }
+        };
+        var mockedDbSet = data.MockData();
+
+        // Act
+        var mockedDbContext = MockExtensions.MockDbContext<TestDbContext>(mockedDbSet);
+
+        // Assert
+        Assert.NotNull(mockedDbContext);
+        Assert.IsType<Mock<TestDbContext>>(mockedDbContext);
+
+        var dbContext = mockedDbContext.Object;
+        var dbSet = dbContext.TestEntities.ToList();
+        Assert.Equal(3, dbSet.Count);
+        Assert.Equal(data, dbSet);
+    }
+
+    [Fact]
+    public void MockDbContext_GiveInvalidData_ThrowArgumentException() {
+        // Arrange
+        var validData = new List<TestEntity> {
+            new() { Id = 1, Name = "Test 1" },
+        };
+        var invalidData = new List<UserNotExistInDbContext> {
+            new() { Id = 1, Name = "Mohammad" }
+        };
+        
+        var mockedDbSetValid = validData.MockData();
+        var mockedDbSetInValid = invalidData.MockData();
+        
+        Assert.Throws<ArgumentException>(() =>
+            MockExtensions.MockDbContext<TestDbContext>(mockedDbSetValid, mockedDbSetInValid));
+    }
+
+    public class TestDbContext : DbContext {
+        public virtual DbSet<TestEntity> TestEntities { get; set; }
+    }
+
     public class TestEntity {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UserNotExistInDbContext {
         public int Id { get; set; }
         public string Name { get; set; }
     }
--- src/TestAssist/MockExtensions.cs
+++ src/TestAssist/MockExtensions.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using MockQueryable.Moq;
 using Moq;
@@ -7,4 +9,74 @@ namespace TestAssist;
 public static class MockExtensions {
     public static Mock<DbSet<TEntity>> MockData<TEntity>(this IEnumerable<TEntity> data) where TEntity : class =>
         data.BuildMock().BuildMockDbSet();
+    
+    public static Mock<TDbContext> MockDbContext<TDbContext>(params Mock[] dataMocks) where TDbContext : DbContext {
+        // Find all properties of type DbSet<T> in the DbContext
+        var dbSetProperties = GetDbSetProperties<TDbContext>();
+
+        var mockContext = new Mock<TDbContext>();
+        foreach (var dataMock in dataMocks)
+            mockContext.SetupMockContext(dataMock, dbSetProperties);
+
+        return mockContext;
+    }
+
+    private static void SetupMockContext<TDbContext>(this Mock<TDbContext> mockContext, Mock dataMock,
+        IEnumerable<PropertyInfo> dbSetProperties)
+        where TDbContext : DbContext {
+        var dbSetPropertyInfo = GetDbSetPropertyInfo<TDbContext>(dataMock, dbSetProperties);
+
+        // mockContext.Setup(x=> x.DbSetProp).Returns(mockData.Object)
+        var setupResult = mockContext.InvokeSetupMethod(dbSetPropertyInfo);
+        InvokeReturnsMethod(setupResult, dataMock, dbSetPropertyInfo);
+    }
+
+    private static void InvokeReturnsMethod(object setupResult, Mock dataMock, PropertyInfo dbSetPropertyInfo) {
+        var returnsMethod = setupResult.GetType().GetMethod("Returns", [dbSetPropertyInfo.PropertyType]);
+        returnsMethod!.Invoke(setupResult, [dataMock.Object]);
+    }
+
+    private static object InvokeSetupMethod<TDbContext>(this Mock<TDbContext> mockContext,
+        PropertyInfo dbSetPropertyInfo)
+        where TDbContext : DbContext {
+        // Create expression for setting up the property
+        // x => x.DbSetProp
+        var parameter = Expression.Parameter(typeof(TDbContext), "x");
+        var propertyAccess = Expression.Property(parameter, dbSetPropertyInfo);
+        var lambda = Expression.Lambda(propertyAccess, parameter);
+
+        // Use reflection to invoke the Setup method
+        var setupMethod = mockContext.GetType().GetMethods().Single(method =>
+            method.ToString()!.Contains("Setup[TResult](System.Linq.Expressions.Expression"));
+        var genericSetupMethod = setupMethod.MakeGenericMethod(dbSetPropertyInfo.PropertyType);
+        var setupResult = genericSetupMethod.Invoke(mockContext, [lambda]);
+
+        return setupResult!;
+    }
+
+    private static PropertyInfo GetDbSetPropertyInfo<TDbContext>(Mock dataMock,
+        IEnumerable<PropertyInfo> dbSetProperties)
+        where TDbContext : DbContext {
+        var dataMockType = dataMock.GetType().GetGenericArguments().First();
+
+        var dbSetPropertyInfo = dbSetProperties.FirstOrDefault(item => item.PropertyType == dataMockType);
+        if (dbSetPropertyInfo is null)
+            throw new ArgumentException(
+                $"Type of '{dataMockType.GetGenericArguments().First()}' is not valid for '{typeof(TDbContext).FullName}'");
+
+        return dbSetPropertyInfo;
+    }
+
+    private static List<PropertyInfo> GetDbSetProperties<TDbContext>() where TDbContext : DbContext {
+        var dbContextType = typeof(TDbContext);
+        var dbSetType = typeof(DbSet<>);
+
+        var dbSetProperties = dbContextType.GetProperties()
+            .Where(prop =>
+                prop.PropertyType.IsGenericType &&
+                prop.PropertyType.GetGenericTypeDefinition() == dbSetType
+            ).ToList();
+
+        return dbSetProperties;
+    }
 }
